ff6bb2efe41483a9b92cbe12a2e6631fe86148df
Patrick Stepien <patrick.stepien@kellogg.northwestern.edu>
2023-04-21 19:06:50 +0000

    practice

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,33 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+#puts 5
+#puts 3
 
 # Perform simple math with numbers
+#puts 5*3
+#puts 5.0/3.0
 
 # Strings
+#puts "Hello, world!"
 
 # Combine strings together
+#puts "Tacos " + "are awesome"
+#puts "tacos" + 3.to_s
 
 # Variables
+# food = "tacos"
+# puts food
+
+# x = 5
+# y = 3
+# puts x+y
 
 # Combine strings and variables
+# puts "#{x} tacos"
+# puts "#{x+y} tacos"
 
 # String manipulation
+# puts "Hello".length
+# puts "How are you today?".upcase
+# puts "computers are hard".reverse
--- labs/1-data.rb
+++ labs/1-data.rb
@@ -15,3 +15,11 @@
 # rand(1..6)
 # Log output to the screen with puts:
 # puts "Hello, world"
+
+die_1 = rand(1..6)
+die_2 = rand(1..6)
+
+puts "The first die rolled showed a #{die_1}"
+puts "The second die rolled showed a #{die_2}"
+puts "The total value rolled was a #{die_1 + die_2}"
+
