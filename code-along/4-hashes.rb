cbaeb59176b36fcc3812a94850255daa7fe01c7f
Patrick Stepien <patrick.stepien@kellogg.northwestern.edu>
2023-04-21 20:37:06 +0000

    practice

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,18 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+me = {
+"name" => "Patrick", 
+"location" => {"city" => "Chicago", "state" => "Illinois"},
+"timeline" => [
+    {"status" => "Student at Kellogg", "posted" => "7:30am"},
+    {"status" => "Taking a nap", "posted" => "3:30pm"}
+]
+}
 
 # Accessing data from the hash
+puts me["name"]
+puts me["location"]["city"] + ", " + me["location"]['state']
 
 # More Complex Hashes
+# JSON is an array of hashes
