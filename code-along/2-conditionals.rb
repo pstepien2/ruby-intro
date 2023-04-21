aa732f8c6ef344bc178074b8e70e589efd75ecf8
Patrick Stepien <patrick.stepien@kellogg.northwestern.edu>
2023-04-21 19:26:09 +0000

    practice

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,44 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+# this_is_true = true
+# this_is_false = false
 
 # Boolean Expressions
+# puts 3 == 2
 
 # If Conditional Logic
+# if 3 > 2 
+#     puts "yes it is"
+# end
 
 # If/Else Conditional Logic
+# user_entered_password = "tacos"
+# real_password = "secret"
+
+# if user_entered_password == real_password
+#     puts "you're in!"
+# else
+#     puts "no way!"
+# end
 
 # Elsif Conditional Logic
+# your_score = 1
+# opponet_score = 2
+
+# if your_score > opponet_score
+#     puts "yay"
+# elsif your_score == opponet_score
+#     puts "ok, you tied"
+# else
+#     puts "sadness"
+# end
+
 
 # Combining Expressions
+temp = 68
+precip = 0
+if temp >= 66 && temp <= 90 && precip == 0
+    puts "it's nice"
+end
+
